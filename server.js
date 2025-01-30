b1b7be74fa58f978825b71645bffdd1e2966bfeb
Itai Schvartzbard <itai@practitest.com>
2025-01-31 00:06:26 +0200

    Taking vars from .env, login to DB

--- server.js
+++ server.js
@@ -3,17 +3,28 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-const port = 3000;
+// Routes:
+import authRoutes from './routes/auth.js';
+// import documentRoutes from './routes/documents.js';
 
 // app
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(authRoutes)
+dotenv.config();
+
+const { DB_URL, PORT } = process.env;
+mongoose.connect(DB_URL)
+    .then(() => console.log("DB Connected"))
+    .catch(err => console.error(err));
+// Routes
+
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
 })
